e429d7067950380d4fe25ad841c39b9780004eaa
sciphi-123 <ryuni2123@gmail.com>
2025-03-19 09:05:23 +0800

    Make minor edits

--- src/test/java/seedu/address/logic/commands/PriorityCommandTest.java
+++ src/test/java/seedu/address/logic/commands/PriorityCommandTest.java
@@ -9,22 +9,22 @@ import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FOURTH_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalPersons.getTypicalWealthVault;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
-import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.WealthVault;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
 
 public class PriorityCommandTest {
 
-    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Model model = new ModelManager(getTypicalWealthVault(), new UserPrefs());
 
     private void execute_togglePriorityUnfilteredList(Index personIndex, String... tags) {
         Person person = model.getFilteredPersonList().get(personIndex.getZeroBased());
@@ -35,7 +35,7 @@ public class PriorityCommandTest {
         String expectedMessage = String.format(PriorityCommand.MESSAGE_PRIORITY_PERSON_SUCCESS,
                 Messages.format(priorityPerson));
 
-        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        Model expectedModel = new ModelManager(new WealthVault(model.getWealthVault()), new UserPrefs());
         expectedModel.setPerson(person, priorityPerson);
 
         assertCommandSuccess(priorityCommand, model, expectedMessage, expectedModel);
